2f830d5782721aa5fe58f5a6c74a1bd5da50e6be
Pat3l <85874958+Pat3l@users.noreply.github.com>
2021-07-16 16:51:26 +0530

    Add files via upload

---
+++ resume_scanner.py
@@ -0,0 +1,6 @@
+def resume_scanner(resume):
+    from resume_parser import resumeparse
+    data = resumeparse.read_file(resume)
+    for i, j in data.items():
+        print(f"{i} ==> {j}")
+        
